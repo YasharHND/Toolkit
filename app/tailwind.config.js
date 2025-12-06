3d2441d4aa96d9d3b57960009e7d5255b9a88690
YasharHND <yashar.hnd.65@gmail.com>
2025-12-06 23:34:51 +0400

    style: tomorrow-font

--- app/tailwind.config.js
+++ app/tailwind.config.js
@@ -3,6 +3,9 @@ export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {
+      fontFamily: {
+        sans: ['Tomorrow', 'system-ui', 'sans-serif'],
+      },
       colors: {
         orange: {
           50: '#fff7ed',
